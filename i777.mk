eadabe745a9c380a9d3e75b887ba2a876954e0d2
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-12-22 01:12:55 +0500

    Remove useless NFC stuff

--- i777.mk
+++ i777.mk
@@ -57,29 +57,11 @@ PRODUCT_PACKAGES += \
 	nfc.exynos4 \
 	Tag
 
-# Commands to migrate prefs from com.android.nfc3 to com.android.nfc
 PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
 packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt)
 
-# file that declares the MIFARE NFC constant
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
-
-# NFC EXTRAS add-on API
-PRODUCT_PACKAGES += \
-	com.android.nfc_extras
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    	NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    	NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
-
-PRODUCT_COPY_FILES += \
-    	$(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+	frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 $(call inherit-product-if-exists, vendor/samsung/i777/i777-vendor.mk)
